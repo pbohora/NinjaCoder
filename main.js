94aa9b71594ab50ca3888d98c3debf5705363b56
pbohora <pradeepbohora26@gmail.com>
2019-03-25 22:14:47 +0200

    added navbar styles that changes on scroll

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const menuIcon = document.getElementById("menu-icon");
 const slideoutMenu = document.getElementById("slideout-menu");
 const searchIcon = document.getElementById("search-icon");
 const searchBox = document.getElementById("searchbox");
+const navBar = document.querySelector("nav");
 
 searchIcon.addEventListener("click", function() {
   if (searchBox.style.top == "72px") {
     searchBox.style.top = "24px";
+    searchBox.style.opacity = "0";
     searchBox.style.pointerEvents = "none";
   } else {
     searchBox.style.top = "72px";
     searchBox.style.pointerEvents = "auto";
+    searchBox.style.opacity = "1";
   }
 });
 
@@ -20,5 +23,16 @@ menuIcon.addEventListener("click", function() {
   } else {
     slideoutMenu.style.opacity = "1";
     slideoutMenu.style.pointerEvents = "auto";
+  }
+});
+
+// change style of navbar on scroll
+
+window.addEventListener("scroll", function(e) {
+  e.preventDefault();
+  if (window.scrollY >= 150) {
+    navBar.classList.add("nav-scroll");
+  } else {
+    navBar.classList.remove("nav-scroll");
   }
 });
